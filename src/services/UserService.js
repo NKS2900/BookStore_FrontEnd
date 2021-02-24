677eb333b76f61f46bba8e74abcbc2e9687ee2d1
NIJMA SAYYAD <nksayyad2900@outlook.com>
2021-02-25 00:39:14 +0530

    [NIJAM] Add: Login Service added.

---
+++ src/services/UserService.js
@@ -0,0 +1,19 @@
+import axios from 'axios'
+import apiConst from '../apiConstant/userApiConstant.js'
+let LoginService = (data) => {
+
+    try{
+        console.log(data);
+        //const response = axios.post(process.env.REACT_APP_BASE_URL+apiConst.login,loginData);
+        const response = axios.post("https://backend-bookstore.herokuapp.com/bookstore_user/login",data);
+        console.log(response);
+        return response;
+    }
+    catch (error){
+        return error;
+    }
+}
+
+
+
+export default {LoginService, Register}
