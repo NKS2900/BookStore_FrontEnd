7b9c0917f36a089edf4485a981db709cdb0e7e29
NIJMA SAYYAD <nksayyad2900@outlook.com>
2021-02-26 22:55:46 +0530

    [NIJAM] Add: bookCover added using json data with response in bookImage field.

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -4,27 +4,43 @@ import DisplayBook from '../../Component/DisplayBook/DisplayBook.jsx'
 import AppBar from '../../Component/AppBar/Appbar.jsx'
 import { getBookService } from '../../services/BookService'
 
+const bookCoverData=require('../../assets/bookCover.json');
+
+console.log(bookCoverData.bookCovers[0].id);
+
 const Home = () => {
     const [bookList, setBookList] = useState([]);
 
-    const GetBooks = () => {
+    const getBooks = () => {
         getBookService().then((response) =>{
             if(response.status === 200){
                 setBookList(response.data.result);
+               // console.log(response.data.result[0]._id);
                 console.log(response);
             }
         }).catch(()=> {
             console.log("Error while Fetching Notes!!!")
         });
     }
+    var books=bookList.length;
+
+    for (let i = 0; i < bookList.length; i++) {   
+        for (let j = 0; j < bookCoverData.bookCovers.length; j++) {                   
+            if (bookList[i]._id===bookCoverData.bookCovers[j].id) {         
+                bookList[i].bookImage=bookCoverData.bookCovers[j].bookCover;
+                break;
+            }
+        }
+    }
 
     useEffect(()=>{
-        GetBooks()
+        getBooks()
     },[])
+    
     return(
-        <div  >
+        <div  className="homeDiv">
             <AppBar/>
-           <DisplayBook item={bookList} />
+            <DisplayBook item={bookList} books={books} GetBook={getBooks}/>
         </div>
     )
 }
