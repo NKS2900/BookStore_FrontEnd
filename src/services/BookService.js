e82d7cc5f107680e5be8285727721fdf23729c1a
NIJMA SAYYAD <nksayyad2900@outlook.com>
2021-03-01 00:52:44 +0530

    [NIJAM] Add: getCart Items service added.

--- src/services/BookService.js
+++ src/services/BookService.js
@@ -26,3 +26,18 @@ export function addToCart(val) {
         return error;
     }
 }
+
+export function getCartBooks() {
+
+    try{
+        
+        const response = axios.get(process.env.REACT_APP_BOOK_URL+apiConst.getCart,
+            {
+                headers: { 'x-access-token': localStorage.getItem('token') },
+            });
+        return response;
+    }
+    catch (error){
+        return error;
+    }
+}
