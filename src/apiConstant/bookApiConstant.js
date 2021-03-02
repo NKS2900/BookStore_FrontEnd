0058be2dae8b2002e323f837acd966c0a0a809d4
NIJMA SAYYAD <nksayyad2900@outlook.com>
2021-03-02 14:00:48 +0530

    [NIJAM] Add: API constant added for Update Book quntity and update address.

--- src/apiConstant/bookApiConstant.js
+++ src/apiConstant/bookApiConstant.js
@@ -1,4 +1,8 @@
 module.exports={
     getbook:"/get/book",
-
+    addtocart:"/add_cart_item/",
+    getCart:"/get_cart_items",
+    remove:"/remove_cart_item/",
+    update:"/cart_item_quantity/",
+    address:"/edit_user",
 }
